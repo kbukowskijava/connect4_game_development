177b416f98ba21c06052389f6fa5152ad1ae949e
Kacper Bukowski <kacper.bukowski@student.wat.edu.pl>
2022-05-09 20:20:35 +0200

    Initial commit

---
+++ config.py
@@ -0,0 +1,25 @@
+# colors
+BLUE = (0, 152, 236)
+RED = (236, 0, 0)
+YELLOW = (255, 224, 22)
+ORANGE = (255, 140, 0)
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+
+# Row/columns
+ROWS = 6
+COLUMNS = 7
+
+# discs
+DISC_SIZE = 80
+DISC_RADIUS = int(DISC_SIZE / 2)
+
+# additional data
+PLAYER = 0
+AI_PLAYER = 1
+
+EMPTY = 0
+PLAYER_PIECE = 1
+AI_PLAYER_PIECE = 2
+
+MAIN_WINDOW = 4
